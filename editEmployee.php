5f3a1823f5eb59c2163f5f006e148d94ff8f0e5a
mariabee <36023257+mariabee@users.noreply.github.com>
2023-08-11 11:42:27 -0400

    formatted things a bit more

--- editEmployee.php
+++ editEmployee.php
@@ -57,28 +57,115 @@ $result = mysqli_query($conn, $query) or die ( mysqli_error($conn));
             <a href="Emails.php">Email Log</a>
         </nav>
     </div>
-    <h1>Edit Employee</h1>
-    <?php
-$status = "";
-$row = mysqli_fetch_assoc($result); 
+    <div>
+        <div class="centreContainer">
+        <h1>Edit Employee</h1>
+    </div>
+            <?php
+        $status = "";
+        $row = mysqli_fetch_assoc($result); 
 
 ?>
-    <div>
+    <div class = "centreContainer">
         <form action="SavePerson.php" name="form" method="post">
-            <input name="ID" value="<?php echo $row['ID'] ?>" />
-            <p><input type="text" name="FirstName" placeholder="Enter first name" required value="<?php echo $row['FirstName'];?>" /></p>
-            <p><input type="text" name="LastName" placeholder="last name" required value="<?php echo $row['LastName'];?>" /></p>
-            <p><input type="text" name="Email" placeholder="email" required value="<?php echo $row['Email'];?>" /></p>
-            <p><input type="tel" name="Phone" placeholder="phone number" required value="<?php echo $row['Phone'];?>" /></p>
-            <p><input type="text" name="Address" placeholder="address" required value="<?php echo $row['Address'];?>" /></p>
-            <p><input type="text" name="PostalCode" placeholder="postal code" required value="<?php echo $row['PostalCode'];?>" /></p>
-            <p><input type="text" name="City" placeholder="city" required value="<?php echo $row['City'];?>" /></p>
-            <p><input type="text" name="Province" placeholder="province" required value="<?php echo $row['Province'];?>" /></p>
-            <p><input type="date" name="CardExpiry" placeholder="card expiry" required value="<?php echo $row['CardExpiry'];?>" /></p>
-            <p><input type="date" name="DateOfBirth" placeholder="date of birth" required value="<?php echo $row['DateOfBirth'];?>" /></p>
-            <p><input type="text" name="citizenship" placeholder="citizenship" required value="<?php echo $row['citizenship'];?>" /></p>
-            <p><input name="submit" type="submit" value="Save Changes" /></p>
+        <p>
+            Card Number <input name="ID" value="<?php echo $id ?>" />
+            Expiry <input type="date" name="CardExpiry" placeholder="card expiry" required value="<?php echo $row['CardExpiry'];?>" /></p>
+        <p>
+            <input type="text" name="FirstName" placeholder="Enter first name" required value="<?php echo $row['FirstName'];?>" />
+            <input type="text" name="LastName" placeholder="last name" required value="<?php echo $row['LastName'];?>" />
+            <input type="text" name="Email" placeholder="email" required value="<?php echo $row['Email'];?>" />
+        </p>
+        <p><input type="tel" name="Phone" placeholder="phone number" required value="<?php echo $row['Phone'];?>" />
+            <input type="text" name="Address" placeholder="address" required value="<?php echo $row['Address'];?>" />
+            <input type="text" name="PostalCode" placeholder="postal code" required value="<?php echo $row['PostalCode'];?>" />
+        </p>
+        <p><input type="text" name="City" placeholder="city" required value="<?php echo $row['City'];?>" />
+            <input type="text" name="Province" placeholder="province" required value="<?php echo $row['Province'];?>" />
+            <input type="text" name="citizenship" placeholder="citizenship" required value="<?php echo $row['citizenship'];?>" />
+        </p>
+            <p>Birth date <input type="date" name="DateOfBirth" placeholder="date of birth" required value="<?php echo $row['DateOfBirth'];?>" /></p>
+            <input class="button purple_bg" name="submit" type="submit" value="Save Changes" />
+        <p><a href="DeletePerson.php?id=<?php echo $row["ID"]; ?>" class="dark_bg">Delete Employee</a></p>
         </form>
+    </div> 
+        <h3>Vaccine History</h3>
+    <table style="border: 1px solid;" width="50%" border="1" style="border-collapse:collapse;">
+            <thead>
+                <tr>
+                    <th><strong>Type</strong></th>
+                    <th><strong>Dose</strong></th>
+                    <th><strong>Date</strong></th>
+                </tr>
+            </thead>
+    
+        <tbody>
+        <?php
+                $sel_query="Select * from gdc353_1.Vaccine V
+                WHERE V.PatientID = ".$id.";";
+                $result = mysqli_query($conn,$sel_query);
+                while($row = mysqli_fetch_assoc($result)) { ?>
+            <tr>
+                <td align="center"><?php echo $row["Type"]; ?></td>
+                <td align="center"><?php echo $row["Dose"]; ?></td>
+                <td align="center"><?php echo $row["Date"]; ?></td>
+                <td align="center">
+                <a href="deleteVaccine.php?dose=<?php echo $row["Dose"]; ?>" class="dark_bg">Delete</a>
+                </td>
+            </tr>
+            <?php } ?>  
+            <form action = "saveVaccine.php" name="form" method="post">
+            <tr> 
+                <input type = "hidden" name="ID" value="<?php echo $id ?>" />
+                <td><input type="text" name="Type" placeholder="Type" /></td>
+                <td><input type="text" name="Dose" placeholder="Dose" /></td>
+                <td><input type="date" name="Date" placeholder="Date" /></td>
+            <tr> 
+            <tr>
+                <td>  
+                    <input type = "submit" class="purple_bg button"></input>
+                </td> 
+            </tr> 
+                </form> 
+        </tbody> 
+    </table> 
+            
+    <h3>Infection History</h3> 
+    <input type = "hidden" name="ID" value="<?php echo $id ?>" />
+    <table style="border: 1px solid;" width="50%" border="1" style="border-collapse:collapse;">
+            <thead>
+                <tr>
+                    <th><strong>Type</strong></th>
+                    <th><strong>Date</strong></th>
+                    <th></th> 
+                </tr>
+            </thead>
+    <tbody>
+    <?php
+            $sel_query="Select * from gdc353_1.Infection I
+            WHERE I.PatientID = ".$id.";";
+            $result = mysqli_query($conn,$sel_query);
+            while($row = mysqli_fetch_assoc($result)) { ?>
+        <tr>
+            <td align="center"><?php echo $row["Type"]; ?></td>
+            <td align="center"><?php echo $row["Date"]; ?></td>
+            <td align="center">
+            <a href="deleteVaccine.php?date=<?php echo $row["Date"]; ?>" class="dark_bg">Delete</a>
+            </td>
+        </tr>
+        <?php } ?>  
+        <form action = "saveInfection.php" name="form" method="post">
+        <tr> 
+            <td><input type="text" name="Type" placeholder="Type" /></td>
+            <td><input type="date" name="Date" placeholder="Date" /></td>
+            <td>  
+                <input type = "submit" class="purple_bg button"></input>
+            </td>
+        </tr>  
+        </form> 
+    </tbody> 
+    </table> 
+    <div class = "centreContainer">
         <h2> Contracts </h2>
         <?php  
 while($row) { ?>
@@ -92,8 +179,8 @@ while($row) { ?>
         &id=
         <?php echo $row["startDate"];?> 
     ">Delete</a> </button>
+    <br> 
         </div>
-        <br>
         <?php
     $row = mysqli_fetch_assoc($result); 
     
